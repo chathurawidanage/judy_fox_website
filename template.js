ee2192f0af389af759b86956f5f664bcefc480a0
Chathura Widanage <chathurawidanage@gmail.com>
2019-10-18 14:54:10 -0400

    Adding project page

--- template.js
+++ template.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
-    $('body').prepend("<div data-load=\"header.html\"></div>");
-    $('body').append("<div data-load=\"footer.html\"></div>");
+    var loc = window.ov_loc ? window.ov_loc : ".";
+    $('body').prepend('<div data-load="' + loc + '/header.html"></div>');
+    $('body').append('<div data-load="' + loc + '/footer.html"></div>');
 
     $("[data-load]").each(function () {
+        console.log(this);
         $(this).load($(this).data("load"), function () {
         });
     });
